e3f7536640220d428aead3efd8529c782f6e0ff2
Luigi Ranghetti <6874239+ranghetti@users.noreply.github.com>
2020-04-29 08:52:11 +0200

    Histogram of frequency

--- inst/apps/guinterp/server/update_interp_histogram.R
+++ inst/apps/guinterp/server/update_interp_histogram.R
@@ -7,13 +7,14 @@ observeEvent(rv$change_interp, {
     req(rv$inputpts_points, rv$borders_polygon)
     rv$hist_range  <- quantile(rv$inputpts_points$selvar, c(.005,.995), type=1)
     rv$hist_breaks <- hist(plot = FALSE, rv$inputpts_points[sid<=samplesize & selvar>=rv$hist_range[1] & selvar<=rv$hist_range[2],]$selvar, breaks=50)$breaks
-    rv$hist_ylim   <- c(0,max(hist(plot = FALSE, rv$inputpts_points[sid<=samplesize & selvar>=rv$hist_range[1] & selvar<=rv$hist_range[2],]$selvar, breaks=50)$counts))
+    rv$hist_ylim   <- c(0,max(hist(plot = FALSE, rv$inputpts_points[sid<=samplesize & selvar>=rv$hist_range[1] & selvar<=rv$hist_range[2],]$selvar, breaks=50)$counts)) /
+      nrow(rv$inputpts_points[sid<=samplesize & selvar>=rv$hist_range[1] & selvar<=rv$hist_range[2],])
 
     p <- ggplot2::ggplot(rv$inputpts_points, ggplot2::aes(x=selvar)) +
-      ggplot2::geom_histogram(data=rv$inputpts_points[sid<=samplesize,], colour="white", aes(fill=filter), breaks=rv$hist_breaks) +
+      ggplot2::geom_histogram(data=rv$inputpts_points[sid<=samplesize,], colour="white", aes(y = stat(count) / sum(count), fill=filter), breaks=rv$hist_breaks) +
       ggplot2::scale_fill_manual(values = c("FALSE"="darkgreen", "TRUE"="red")) +
-      ggplot2::ylim(rv$hist_ylim) +
-      ggplot2::xlab(i18n$t("_hist_xlab")) + ggplot2::ylab(i18n$t("_hist_ylab"))
+      ggplot2::scale_y_continuous(labels = scales::percent, limits = rv$hist_ylim) +
+      ggplot2::xlab(gsub("%v",input$select_inputvar,i18n$t("_hist_xlab"))) + ggplot2::ylab(i18n$t("_hist_ylab"))
     if (!is.null(input$miny) & !is.null(input$maxy) & input$check_rangey & input$filter_buttons == "manual") {
       p <- p + ggplot2::geom_vline(xintercept = c(input$miny, input$maxy), colour="red")
     }
