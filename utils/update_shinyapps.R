1b2462ad600ad0d3792697e58cef68ae7c840478
Luigi Ranghetti <6874239+ranghetti@users.noreply.github.com>
2020-06-03 11:20:49 +0200

    v 0.3.0 (#19)
    Update documentation

--- utils/update_shinyapps.R
+++ utils/update_shinyapps.R
@@ -1,12 +1,24 @@
 
-# Before running this, rebase branch "shinyapps" on "develop"
+# BEFORE RUNNING THE SCRIPT:
+#
+# replace the following chunk in guinterp.R:
+#
+# shinyOptions(
+#   ui_lang = language,
+#   demo_mode = demo
+# )
+#
+# with:
+#
+# shinyOptions(
+#   ui_lang = "en",
+#   demo_mode = TRUE
+# )
 
-# remotes::install_github("ranghetti/guinterp", ref = "develop")
 remotes::install_github("r-spatial/lwgeom", ref = "467dc314a0bda011e806a8b56c8dcc153f63527c")
 devtools::load_all(".") # local branch shinyapps
 rsconnect::appDependencies(system.file(package="guinterp"))
 rsconnect::deployApp(
-  # system.file(package="guinterp"),
   appName = "guinterp",
   appTitle = "GUInterp: a Shiny GUI for Spatial Interpolation",
   appId = "2387850",
